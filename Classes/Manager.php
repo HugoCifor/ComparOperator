4c9f946a35b7cf1b85a9af32d96a9cd602b6c76e
hugo <hugo.cifor@gmail.com>
2022-04-01 14:18:41 +0200

    avance 2

--- Classes/Manager.php
+++ Classes/Manager.php
@@ -6,15 +6,33 @@ class Manager
 
     function __construct($data)
     {
+        $this->db = new DataBase("comparoperator");
 
     }
 
     
     function getAllDestination()
     {
-
+        $req=$this->db->getPDO()->prepare('SELECT * FROM destination');
+        $req->execute([]);
+        $AllDest=$req->fetchAll();
+        return $AllDest;
     }
 
+    function getDestinationNames()
+    {
+        $allNames=[];
+        $AllDest= $this->getAllDestination();
+        foreach ($AllDest as $value) {
+            foreach ($value as $names => $name) {
+               if ($names==='location')
+                array_push($allNames,$name);
+            }
+            
+            
+        }
+        return $allNames;
+    }
 
     function getOperatorByDestination()
     {
