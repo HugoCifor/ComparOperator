99832b7eab845dc95b819020f9e2373ac9b410ee
deveauxsarah <deveauxsarah.dev42@gmail.com>
2022-04-01 09:24:45 +0200

    ok

---
+++ Manager.php
@@ -0,0 +1,24 @@
+<?php
+
+class Manager
+{
+    private $bdd;
+
+
+    function __construct()
+    {
+
+    }
+
+}
+
+// getAllDestination();
+// getOperatorByDestination();
+// createReview();
+// getReviewByOperatorId();
+
+
+// getAllOperator();
+// updateOperatorToPremium();
+// createTourOperator();
+// createDestination();
---
+++ tourOperator.php
@@ -0,0 +1,27 @@
+<?php
+
+class TourOperator
+{
+
+    private $id;
+    private $name;
+    private $link;
+    private $gradeCount;
+    private $gradeTotal;
+    private $isPremium;
+
+
+    function __construct($data)
+    {
+
+    }
+
+}
+
+// getId();
+// getName();
+// getLink();
+// getGradeCount();
+// getGradeTotal();
+// getGrade();
+// getPremium();
