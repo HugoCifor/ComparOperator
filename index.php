0641ee5738380c468d117346c5a204f4a1b03052
hugo <hugo.cifor@gmail.com>
2022-04-01 13:17:14 +0200

    truc

--- index.php
+++ index.php
@@ -9,18 +9,22 @@ include './config/autoload.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
     <title>comparOperator</title>
 </head>
 <body>
     <h1>welcome to the comparOperator</h1>
-    <?php $data = 'oui';
+    <?php $data = 'oui'; // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     $classe = new Destination($data);
     $info = $classe ->getDestinationNames();
+    ?>
+    <ul><?php
+        foreach ($info as $key => $value) {
+            echo "<li>".$value."</li>";
+        }
+    ?>
+    </ul>
 
-foreach ($info as $key => $value) {
-    echo $value."</br>";
-}
-?>
 
 </body>
 </html>
