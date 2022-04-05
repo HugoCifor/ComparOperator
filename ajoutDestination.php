7353036ced7228e8c49a6859c266070afcbaf983
hugo <hugo.cifor@gmail.com>
2022-04-05 15:30:37 +0200

    oui

--- ajoutDestination.php
+++
@@ -1,3 +0,0 @@
-<?php
-include "../config/bdd.php";
-include '../config/autoload.php';
--- selected_destination.php
+++ selected_destination.php
@@ -14,28 +14,22 @@ include './config/autoload.php';
  <body>
      <?php
 $manager = new Manager();
-$TOs = $manager->getTOName($_GET['dest']);
+$TOs = $manager->getTONamesByDest($_GET['dest']);
 
 var_dump($TOs)
 ?>
      <div class="TOResult">
-        
-           
-            
-            <?php foreach ($TOs as $key => $value) {
-                $set= new Manager();
-                $donnee = $set-> prepDataForTO($value);
-                $test = new TourOperator($donnee);
-                $result = $test->getPremium();
-                if ($result==1) {
-                    ?><div class="TO"><a href=<?=$donnee['link']?>><?=$donnee['name']?></a></div><?php 
-                }else{
-                    ?><div class="TO"><?=$value?></div> <?php
-                }
-                
-
-            }?>
-            
+        <?php foreach ($TOs as $key => $value) {
+            $set= new Manager();
+            $donnee = $set-> prepDataForTO($value);
+            $premium = new TourOperator($donnee);
+            $result = $premium->getPremium();
+            if ($result==1) {
+                ?><div class="TO"><a href=<?=$donnee['link']?>><?=$donnee['name']?></a></div><?php 
+            }else{
+                ?><div class="TO"><?=$value?></div> <?php
+            }       
+        }?>         
      </div>
 
 
