ba3aab37df52341171180b1c5417705c217762b8
hugo <hugo.cifor@gmail.com>
2022-04-08 20:01:35 +0200

    bug fix duplication de la page destination et reparation du prix

--- selected_destination.php
+++ selected_destination.php
@@ -276,25 +276,7 @@ if (isset($_GET['dest'])&&in_array($_GET['dest'],$control)) {
             </table>
         </section>
     </main>
-<footer id="footer" class="align-content text-white text-center font-medium p-10">
-    <div class="flex flex-row ">
-        <a href="#top"><img id="butTop" class="w-12 " src="./images/Capture.PNG" alt="Retour en haut"></a>
-    </div>
-    <div class="flex flex-row justify-around ">
-        <div id="developpement">
-            <h3>Développement :</h3><br>SARL ANTHEDESIGN<br>Adresse : 12 Rue du Huit Mai 1945, 60350 ATTICHY<br>Site Web : www.anthedesign.fr
-        </div>
-        <div id="hebergement">
-            <h3>Hébergement :</h3><br>Hébergeur : SARL ANTHEDESIGN<br>12 Rue du Huit Mai 1945, 60350 ATTICHY<br>Site Web : www.anthedesign.fr
-        </div>        
-        <div id="editor">
-            <h3>Éditeur du Site :</h2><br>SARL ANTHEDESIGN Numéro de SIRET : 75221735600027<br>Responsable éditorial : Hugo ESSIQUE<br>12 Rue du Huit Mai 1945, 60350 ATTICHY<br>Téléphone : 09 72 21 25 07<br>Email : contact@anthedesign.fr<br>Site Web : www.anthedesign.fr
-        </div>
-    </div>
-	<div><br>
-        <div  class="text-center text-gray-900">Production : DEVEAUX Sarah et LAMURE Hugo _2022_ </div> 
-    </div>
-</footer> 
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="/JS/main.js"></script>
